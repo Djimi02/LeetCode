a06548d5474052c20c73dfddecaf8dfe6ceea146
Djimi02 <mitko.manolev1@abv.bg>
2024-06-08 19:38:01 +0200

    three sum solution optimized using 3-pointers

--- LeetCodeProblemSolutions/ThreeSum.java
+++ LeetCodeProblemSolutions/ThreeSum.java
@@ -1,67 +1,65 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ThreeSum {
-    
-    /** 
-     * @param list
-     * @param set
-     * @return List<List<Integer>>
-     */
-    public List<List<Integer>> checkIfTripletExists(List<List<Integer>> list, List<Integer> set){
-        if (set.get(0) == 0 &&
-            set.get(1) == 0 &&
-            set.get(2) == 0 &&
-            !list.contains(set)) {
 
-            list.add(set);
-            return list;
-        }
-
-        for (List<Integer> set1 : list) {
-            if (set1.contains(set.get(0)) &&
-                set1.contains(set.get(1)) &&
-                set1.contains(set.get(2))) {
-                return list;
+    private boolean checkIfTripletExists(List<List<Integer>> list, List<Integer> triplet){
+        for (List<Integer> triplet1 : list) {
+            if (triplet1.get(0) == triplet.get(0) &&
+                triplet1.get(1) == triplet.get(1) &&
+                triplet1.get(2) == triplet.get(2)) {
+                return true;
             }
         }
-        list.add(set);
-        return list;
+        return false;
     }
-
     
-    /** 
-     * @param nums
-     * @return List<List<Integer>>
-     */
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> output = new ArrayList<>(){};
+        // Sorting numbers
+        nums = Arrays.stream(nums).sorted().toArray();
+        System.out.println(Arrays.toString(nums));
+
+        List<List<Integer>> output = new ArrayList<>();
 
         for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                for (int j2 = j + 1; j2 < nums.length; j2++) {
+            // Skip duplicate elements for i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    ArrayList<Integer> triplet = new ArrayList<>();
+                    triplet.add(nums[i]);
+                    triplet.add(nums[left]);
+                    triplet.add(nums[right]);
 
-                    if (nums[i] + nums[j] + nums[j2] == 0) {
-                        List<Integer> set = new ArrayList<>();
-
-                        set.add(nums[i]);
-                        set.add(nums[j]);
-                        set.add(nums[j2]);
-                        output = checkIfTripletExists(output, set);
+                    if (!checkIfTripletExists(output, triplet)) {
+                        output.add(triplet);
                     }
+
+                    left++;
+                    right--;
+                }
+                else if (sum < 0) {
+                    left++;
+                }
+                else {
+                    right--;
                 }
             }
         }
+
         return output;
     }
 
-    
-    /** 
-     * @param args
-     * 
-     */
+
     public static void main(String[] args) {
-        List<List<Integer>> output = new ThreeSum().threeSum(new int[]{-1,0,1,0,0});
+        List<List<Integer>> output = new ThreeSum().threeSum(new int[]{-1,0,1,2,-1,-4});
         for (List<Integer> list : output) {
             System.out.println(list);
         }
